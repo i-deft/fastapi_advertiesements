bc986bc48281c9fad9009bae2b28e8695c99375e
dmitriy.zagidullin <dmitriy.zagidullin@myoffice.team>
2023-05-22 11:55:31 +0300

    FA-001 | fix: try to run app

---
+++ README.md
@@ -0,0 +1,17 @@
+// add readme to install and start app
+
+## venv
+...
+
+
+## install
+```
+pip install -r requirements.txt
+```
+
+
+## start
+
+```
+uvicorn main:app --reload
+```
--- app/db/database.py
+++ app/db/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .db_settings import db_settings
 
-SQLALCHEMY_DATABASE_URL = db_settings
+SQLALCHEMY_DATABASE_URL = 'sqlite://'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
